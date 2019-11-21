8eb6d6c2e63c521ac3dd4bb9433d12e61855de0b
Mitchell <jmitchell@grupoassa.com>
2019-11-21 14:38:14 -0300

    Add arraylist and modify the creation of json files

--- JsonJava/src/main/java/JsonToJava.java
+++ JsonJava/src/main/java/JsonToJava.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonToJava {
@@ -14,22 +16,27 @@ public class JsonToJava {
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery("select * from CustomerInfo where purchasedDate=CURDATE() and Location ='Asia';");
 
-        CustomerInfo customerInfo = new CustomerInfo();
+        ArrayList<CustomerInfo> listCustomerInfo = new ArrayList();
 
         while(rs.next()){
+            CustomerInfo customerInfo = new CustomerInfo();
             customerInfo.setCourseName(rs.getString(1));
             customerInfo.setPurchasedDate(rs.getString(2));
             customerInfo.setAmount(rs.getInt(3));
             customerInfo.setLocation(rs.getString(4));
+            listCustomerInfo.add(customerInfo);
         }
 
         conn.close();
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        for(int i=0; i < listCustomerInfo.size(); i++){
+            ObjectMapper objectMapper = new ObjectMapper();
 
-        Path currentRelativePath = Paths.get("");
-        String jsonPath = currentRelativePath.toAbsolutePath().toString() + "\\jsonfiles\\customerInfo.json";
+            Path currentRelativePath = Paths.get("");
+            String jsonPath = currentRelativePath.toAbsolutePath().toString() + "\\jsonfiles\\customerInfo" + i + ".json";
 
-        objectMapper.writeValue(new File(jsonPath), customerInfo);
+            objectMapper.writeValue(new File(jsonPath), listCustomerInfo.get(i));
+        }
+
     }
 }
