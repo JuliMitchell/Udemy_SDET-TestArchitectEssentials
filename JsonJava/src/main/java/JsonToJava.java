eaffdf3417478349577f0b1046111f45127c63f1
Mitchell <jmitchell@grupoassa.com>
2019-11-21 11:48:57 -0300

    I add the Jackson API dependecy in pom.xml. I import the library in JsonToJava class and use it to create a json file in jsonFile folder.

--- JsonJava/src/main/java/JsonToJava.java
+++ JsonJava/src/main/java/JsonToJava.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonToJava {
-    public static void main(String args[]) throws ClassNotFoundException, SQLException {
+    public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException {
         //Import the driver
         Class.forName("com.mysql.cj.jdbc.Driver");
         //Create connection
@@ -9,21 +14,22 @@ public class JsonToJava {
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery("select * from CustomerInfo where purchasedDate=CURDATE() and Location ='Asia' LIMIT 1;");
 
+        CustomerInfo customerInfo = new CustomerInfo();
+
         while(rs.next()){
-
-            CustomerInfo customerInfo = new CustomerInfo();
-
             customerInfo.setCourseName(rs.getString(1));
             customerInfo.setPurchasedDate(rs.getString(2));
             customerInfo.setAmount(rs.getInt(3));
             customerInfo.setLocation(rs.getString(4));
-
-            System.out.println(customerInfo.getCourseName());
-            System.out.println(customerInfo.getPurchasedDate());
-            System.out.println(customerInfo.getAmount());
-            System.out.println(customerInfo.getLocation());
         }
 
         conn.close();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Path currentRelativePath = Paths.get("");
+        String jsonPath = currentRelativePath.toAbsolutePath().toString() + "\\jsonfiles\\customerInfo.json";
+
+        objectMapper.writeValue(new File(jsonPath), customerInfo);
     }
 }
